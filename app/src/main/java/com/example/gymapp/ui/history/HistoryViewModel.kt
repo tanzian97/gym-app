d7c510c6536a579cd59f07bfe8775d6c4c0c752b
Tan Zi An <tanzian97@gmail.com>
2023-03-19 02:27:36 +0000

    Rename Dashboard to History

--- app/src/main/java/com/example/gymapp/ui/history/HistoryViewModel.kt
+++ app/src/main/java/com/example/gymapp/ui/history/HistoryViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.gymapp.ui.dashboard
+package com.example.gymapp.ui.history
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class HistoryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is history Fragment"
     }
     val text: LiveData<String> = _text
 }
