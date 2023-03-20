31cfca83fec8fade5b0285d6c3a2311b43f072a3
Tan Zi An <tanzian97@gmail.com>
2023-03-20 11:42:22 +0000

    Add navigation argument to Workout fragment

--- app/src/main/java/com/example/gymapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gymapp/ui/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -40,7 +39,8 @@ class HomeFragment : Fragment() {
             workoutType?.let {
                 this.findNavController().navigate(
                     HomeFragmentDirections
-                        .actionNavigationHomeToNavigationWorkout(workoutType))
+                        .actionNavigationHomeToNavigationWorkout(workoutType)
+                )
                 homeViewModel.doneNavigating()
             }
         })
