e0879cd2bfb175b3fa2cbb5f998a824be047d48c
Tan Zi An <tanzian97@gmail.com>
2023-03-21 02:23:27 +0000

    Change icons for History and Settings on bottom nav

--- app/src/main/java/com/example/gymapp/ui/history/HistoryViewModel.kt
+++ app/src/main/java/com/example/gymapp/ui/history/HistoryViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HistoryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is history Fragment"
+        value = "This is the History fragment."
     }
     val text: LiveData<String> = _text
 }
